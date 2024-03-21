a54dc262e7013e5a51fa564e9a469d0bc5133afc
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-03-21 13:43:58 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+// Write your solution in this file!const 
+let employee ={
+    name:"Tulley",
+    streetAddress:"1st Avenue",
+
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return {...employee,...{[key]: value} }
+    
+}
+employee=updateEmployeeWithKeyAndValue(employee,"streetAddress", '2nd Avenue');
+console.log(employee);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,  key, value){
+    employee[key]= value;
+    return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee,"name","zach")
+console.log(employee);
+
+function deleteFromEmployeeByKey(employee,key){
+   const newEmployee={...employee};
+   delete newEmployee[key];
+   return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
+
